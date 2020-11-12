2d6ee06dcf9f1627b7ea2d443e19d8b5af065c6c
Iris SF <iris.kosy@gmail.com>
2020-11-12 11:09:40 +0100

    Agregar archivos de proyecto.

---
+++ ServicesVol1_Ejer5Timer/MyTimer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServicesVol1_Ejer5Timer
+{
+    public delegate void Deleg();
+
+    class MyTimer
+    {
+        public Deleg leaderDeleg; //Deleg leaderDeleg = new Deleg(increment);
+        public int interval;
+        public MyTimer(Deleg leaderDeleg)
+        {
+            this.leaderDeleg = leaderDeleg;
+            Thread secondThread = new Thread(run);
+            secondThread.Start();
+        }
+
+        public void run()
+        {
+            bool pause = false;
+            while (!pause)
+            {
+            leaderDeleg(); // llamamos a increment función
+            }
+        }
+
+        public void pause()
+        {
+
+        }
+    }
+
+}
---
+++ ServicesVol1_Ejer5Timer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesVol1_Ejer5Timer
+{
+    class Program
+    {
+        static int counter = 0;
+        static void increment()
+        {
+            //Esta función nos incrementará el valor de counter y lo imprimira en pantalla
+            counter++;
+            Console.WriteLine(counter);
+        }
+        static void Main(string[] args)
+        {
+            MyTimer t = new MyTimer(increment);//le pasamos la función a MyTimer
+            t.interval = 1000;//Y los milisegundos
+
+            string op = "";
+
+            do
+            {
+                Console.WriteLine("Press any key to start.");
+                Console.ReadKey();
+                t.run();
+                Console.WriteLine("Press any key to pause.");
+                Console.ReadKey();
+                t.pause();
+                Console.WriteLine("Press 1 to restart or Enter to end.");
+                op = Console.ReadLine();
+            } while (op == "1");
+        }
+    }
+}
